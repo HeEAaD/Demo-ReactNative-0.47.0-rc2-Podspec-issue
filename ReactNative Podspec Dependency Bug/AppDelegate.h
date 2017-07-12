0aa54f5c003b94a9b03cd7726fa8cffa4dde22e7
Steffen Matthischke <steffen.matthischke@lovoo.com>
2017-07-12 13:02:47 +0200

    Initial Commit

---
+++ ReactNative Podspec Dependency Bug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReactNative Podspec Dependency Bug
+//
+//  Created by Steffen Matthischke on 12.07.17.
+//  Copyright © 2017 Steffen Matthischke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReactNative Podspec Dependency Bug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReactNative Podspec Dependency Bug
+//
+//  Created by Steffen Matthischke on 12.07.17.
+//  Copyright © 2017 Steffen Matthischke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
